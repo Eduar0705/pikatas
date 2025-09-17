783521e5991583d6347249e360b03aaa285ec2a5
unknown <eduarjosesuarezgraterol@gmail.com>
2025-09-17 18:38:06 -0400

    arreglos

--- routers/enviaInfo.js
+++ routers/enviaInfo.js
@@ -2,20 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/enviarInfo', (req, res) => {
-    const { name, subject, message } = req.body;
+    const { nombre, telefono, fecha, hora, comensales, email, comentarios} = req.body;
     
     // Formatear el mensaje con emojis y estructura clara
     const formattedMessage = `📄 *Nuevo mensaje de contacto* 📄\n\n` +
-                            `👤 *Nombre:* ${name}\n` +
-                            `📌 *Asunto:* ${subject}\n\n` +
-                            `📝 *Mensaje:*\n${message}\n\n` +
+                            `👤 *Nombre:* ${nombre}\n` +
+                            `📞 *Telefono:* ${telefono}\n\n` +
+                            `📅 *Fecha:* ${fecha}\n\n` +
+                            `⏰ *Hora:* ${hora}\n\n` +
+                            `🍽️ *Comensales:* ${comensales}\n\n` +
+                            `📧 *Email:* ${email}\n\n` +
+                            `📝 *Mensaje:*\n${comentarios}\n\n` +
                             `_Este mensaje fue enviado desde el formulario de contacto_`;
     
     // Codificar el mensaje para URL
     const encodedMessage = encodeURIComponent(formattedMessage);
     
     // Crear el enlace de WhatsApp
-    const whatsappUrl = `https://wa.me/+50685396632?text=${encodedMessage}`;
     const whatsappUrl1 = `https://wa.me/+584145003573?text=${encodedMessage}`;
     
     // Redirigir al usuario a WhatsApp
